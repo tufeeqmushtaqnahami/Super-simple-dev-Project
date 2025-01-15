97aca341b377753a65ff722672218a4985ac3351
Tufeeq Mushtaq Nahami <tufeeqmushtaq000@gmail.com>
2025-01-15 15:45:48 +0530

    made add to cart button interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,7 +1,4 @@
-let productsHTML = '';
-
-
-
+let productsHTML = "";
 
 products.forEach((product) => {
   productsHTML += `
@@ -17,14 +14,16 @@ products.forEach((product) => {
 
                     <div class="product-rating-container">
                         <img class="product-rating-stars"
-                        src="images/ratings/rating-${product.rating.stars *10}.png">
+                        src="images/ratings/rating-${
+                          product.rating.stars * 10
+                        }.png">
                         <div class="product-rating-count link-primary">
                         ${product.rating.count}
                         </div>
                     </div>
 
                     <div class="product-price">
-                        $${(product.priceCents /100).toFixed(2) }
+                        $${(product.priceCents / 100).toFixed(2)}
                     </div>
 
                     <div class="product-quantity-container">
@@ -49,24 +48,37 @@ products.forEach((product) => {
                         Added
                     </div>
 
-                    <button class="add-to-cart-button button-primary js-add-to-cart data-product-name="${product.name}">
+                    <button class="add-to-cart-button button-primary js-add-to-cart" data-product-id="${
+                      product.id
+                    }">
                         Add to Cart
                     </button>
                     </div>
   `;
 });
 
+document.querySelector(".js-products-grid").innerHTML = productsHTML;
 
+document.querySelectorAll(".js-add-to-cart").forEach((button) => {
+  button.addEventListener("click", () => {
+    const productId = button.dataset.productId;
 
+    let matchingItem;
 
-
-
-
-document.querySelector('.js-products-grid').innerHTML = productsHTML ;
-
-document.querySelectorAll('.js-add-to-cart')
-  .forEach((button) =>{
-    button.addEventListener('click', () =>{
-        console.log(button.dataset);
+    cart.forEach((item) => {
+      if (productId === item.productId) {
+        matchingItem = item;
+      }
     });
-  })
+
+    if (matchingItem) {
+      matchingItem.quantity += 1;
+    } else {
+      cart.push({
+        productId: productId,
+        quantity: 1,
+      });
+    }
+    console.log(cart);
+  });
+});
