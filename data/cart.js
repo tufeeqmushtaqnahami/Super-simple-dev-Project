873f089ab6f6b1d500204d38ad1fc5e151c8124e
Tufeeq Mushtaq Nahami <tufeeqmushtaq000@gmail.com>
2025-01-16 14:05:56 +0530

    move addtocart to cart.js

--- data/cart.js
+++ data/cart.js
@@ -1,3 +1,23 @@
 export const cart = [
     
 ]
+
+
+export function addToCart (productId){
+    let matchingItem;
+  
+    cart.forEach((item) => {
+      if (productId === item.productId) {
+        matchingItem = item;
+      }
+    });
+  
+    if (matchingItem) {
+      matchingItem.quantity += 1;
+    } else {
+      cart.push({
+        productId: productId,
+        quantity: 1,
+      });
+    }
+  }
--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,4 +1,4 @@
-import {cart as myCart} from '../data/cart.js';
+import {cart as cart , addToCart} from '../data/cart.js';
 import { products } from '../data/products.js';
 
 let productsHTML = "";
@@ -62,34 +62,27 @@ products.forEach((product) => {
 
 document.querySelector(".js-products-grid").innerHTML = productsHTML;
 
-document.querySelectorAll(".js-add-to-cart").forEach((button) => {
-  button.addEventListener("click", () => {
-    const productId = button.dataset.productId;
 
-    let matchingItem;
 
-    cart.forEach((item) => {
-      if (productId === item.productId) {
-        matchingItem = item;
-      }
-    });
 
-    if (matchingItem) {
-      matchingItem.quantity += 1;
-    } else {
-      cart.push({
-        productId: productId,
-        quantity: 1,
-      });
-    }
-    
+function updateCartQuantity(){
     let cartQuantity = 0;
-
     cart.forEach((item) => {
     cartQuantity += item.quantity;
     })
+    document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
+} 
 
-   document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
+
+
+  document.querySelectorAll(".js-add-to-cart").forEach((button) => {
+    button.addEventListener("click", () => {
+      const productId = button.dataset.productId;
+      
+      addToCart(productId);
+      updateCartQuantity();
+      
+   
   });
   
 });
